69c47a07016de79eeda0089e68316439883a97cb
g1thub1 <tatianabrit007@gmail.com>
2018-03-13 17:18:59 +0200

    initial commit

---
+++ homework/l11_12/Oop.java
@@ -0,0 +1,8 @@
+package homework.l11_12;
+
+public class Oop {
+    public static void main(String[] args) {
+        String s = "object oriented programming hjgfdsgf";
+        System.out.println(s.replace("object oriented programming", "OOP"));
+    }
+}
