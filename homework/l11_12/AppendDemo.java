18d5be131e8191ac998e9cc040fa7aa149e6ce05
g1thub1 <tatianabrit007@gmail.com>
2018-03-18 18:08:32 +0200

    initial commit

---
+++ homework/l11_12/AppendDemo.java
@@ -0,0 +1,42 @@
+package homework.l11_12;
+
+/**
+ * Дано два числа, например 3 и 56, необходимо составить следующие строки:
+ * 3 + 56 = 59
+ * 3 – 56 = -53
+ * 3 * 56 = 168
+ * Используем метод StringBuilder.append().
+ * Замените символ “=” на слово “равно”.
+ * Используйте методы StringBuilder.insert(), StringBuilder.deleteCharAt().
+ * Замените символ “=” на слово “равно”.
+ * Используйте методы StringBuilder.replace().
+ */
+public class AppendDemo {
+    public static void main(String[] args) {
+        calculate(56, 3);
+    }
+
+    private static void calculate(int a, int b) {
+        String result;
+        StringBuilder strbld = new StringBuilder();
+
+        result = strbld.append(a).append(" + ").append(b).append(" = ").append(a + b).append("\n").toString();
+
+        strbld.append(a);
+        strbld.append(" - ");
+        strbld.append(b);
+        strbld.append(" = ");
+        strbld.append(a - b);
+        strbld.append("\n");
+
+        System.out.println(strbld.append(a).append(" * ").append(b).append(" = ").append(a * b).append("\n"));
+
+        int pos = 0;
+        while ((pos = strbld.indexOf("=")) != -1) {
+            /*strbld.deleteCharAt(pos);
+            strbld.insert(pos, "равно");*/
+            strbld.replace(pos, pos + 1, "равно");
+        }
+        System.out.println(strbld);
+    }
+}
